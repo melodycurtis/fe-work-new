a24a51274cd259117ba94c29c2dc239e6f8da9fc
melodycurtis <melodycurtis123@gmail.com>
2020-02-27 15:06:26 +0000

    new collection idea

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Submit my staff details',
+  serviceName: 'FE workforce collection',
 
   // Default port that prototype runs on
   port: '3000',
