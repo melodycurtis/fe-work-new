2a61a12a81ba141026403c8f7acbfbb1f8e3d38c
melodycurtis <melodycurtis123@gmail.com>
2020-06-19 12:34:41 +0100

    new question on qualifications and programmes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'FE staff collection 21/22',
+  serviceName: 'FE staff collection 20/21',
 
   // Default port that prototype runs on
   port: '3000',
